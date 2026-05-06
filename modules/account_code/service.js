0bf153b6f76aba4f99b136503fd64e83f784aa2c
Md Masud Mia <ranamasud0701@gmail.com>
2026-05-06 11:26:45 +0600

    added the filter received code

--- modules/account_code/service.js
+++ modules/account_code/service.js
@@ -3,7 +3,7 @@ import { withConnection, oracledb } from "../../config/db.js";
 export async function getAccountCodeList() {
   return withConnection(async (connection) => {
     const result = await connection.execute(
-      "SELECT ACCOUNT_ID, ACCOUNT_NAME FROM chart_of_account WHERE lastLevel=1 AND (substr(account_id,1,1)=5 OR substr(account_id,1,1)=3)",
+      "SELECT ACCOUNT_ID, ACCOUNT_NAME FROM chart_of_account WHERE lastLevel=1 AND (substr(account_id,1,1)=5)",
       {},
       { outFormat: oracledb.OUT_FORMAT_OBJECT }
     );
--- modules/rec_account_code/rec_account_code.service.js
+++ modules/rec_account_code/rec_account_code.service.js
@@ -1,16 +1,16 @@
-import { withConnection } from "../../config/db.js";
+import { withConnection, oracledb } from "../../config/db.js";
 
 export async function getRecAccountCodes() {
   const sql = `
     SELECT ACCOUNT_ID, ACCOUNT_NAME
     FROM CHART_OF_ACCOUNT
     WHERE LASTLEVEL = 1
-      AND (SUBSTR(ACCOUNT_ID, 1, 1) = '4' OR SUBSTR(ACCOUNT_ID, 1, 1) = '2')
+      AND (SUBSTR(ACCOUNT_ID, 1, 1) = '4')
     ORDER BY ACCOUNT_ID
   `;
 
   return withConnection(async (conn) => {
-    const result = await conn.execute(sql, {}, { outFormat: 4002 });
+    const result = await conn.execute(sql, {}, {  outFormat: oracledb.OUT_FORMAT_OBJECT });
     return { count: result.rows.length, data: result.rows };
   });
 }

a9dfb94841d0468b2d62c420f515b390ee30e8f1
Md Masud Mia <ranamasud0701@gmail.com>
2026-07-07 01:03:07 +0600

    added the home

--- modules/sale-invoice/route.js
+++ modules/sale-invoice/route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/dashboard/breakdown',       invoiceController.dashboardBreakdown);
 router.get('/dashboard/monthly-summary', invoiceController.dashboardMonthlySummary);
+router.get('/dashboard/daily-summary', invoiceController.dashboardDailySummary);
 
 router.get('/',     invoiceController.getAll);
 router.post('/',    invoiceController.create);

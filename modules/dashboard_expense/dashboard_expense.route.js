5109aaed6841ec2186d18d78f9520fc5795c17e1
Md Masud Mia <ranamasud0701@gmail.com>
2026-07-09 18:36:10 +0600

    added the summary

--- modules/dashboard_expense/dashboard_expense.route.js
+++ modules/dashboard_expense/dashboard_expense.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { getExpense, getExpenseDetails } from "./dashboard_expense.controller.js";
+import { getExpense, getExpenseDetails ,expenseByAccount} from "./dashboard_expense.controller.js";
 
 const router = Router();
 
 // GET /api/dashboard/expense?month=09&year=2025
 router.get("/", getExpense);
+router.get('/by-account', expenseByAccount); // /breakdown route-এর আগে বসাও
 
 // GET /api/dashboard/expense/breakdown?month=09&year=2025
 router.get("/breakdown", getExpenseDetails);
+
 
 export default router;

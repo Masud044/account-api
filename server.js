1c231b3cb3f168bcc0d9b8c56f7566827fca8a94
Md Masud Mia <ranamasud0701@gmail.com>
2026-07-11 12:04:32 +0600

    add

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
-// import "dotenv/config";
-// import app from "./app.js";
-// import { checkDatabaseConnection } from "./config/db.js";
-
-// const port = Number(process.env.PORT || 5000);
-
-// async function start() {
-//   try {
-//     await checkDatabaseConnection();
-//     console.log("Database connected successfully");
-//   } catch (error) {
-//     console.error("Database connection failed:", error.message);
-//   }
-
-//   app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-//   });
-// }
-
-// start();
-
 import "dotenv/config";
 import app from "./app.js";
-import { initDb } from "./config/db.js";
+import { checkDatabaseConnection } from "./config/db.js";
 
 const port = Number(process.env.PORT || 5000);
 
 async function start() {
   try {
-    await initDb();
+    await checkDatabaseConnection();
+    console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error.message);
-    process.exit(1);
   }
 
   app.listen(port, () => {
@@ -39,3 +18,4 @@ async function start() {
 }
 
 start();
+
